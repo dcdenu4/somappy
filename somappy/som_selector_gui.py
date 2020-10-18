9737f9e793805844e0a2cb8b68db3399af34df17
Doug <dcdenu4@gmail.com>
2020-10-18 13:45:02 -0400

    Update requirements and add save / load ability
    set matplotlib to 3.0.3 for displaying plots in the gui. Add save / load
    ability for the input parameters.

--- somappy/som_selector_gui.py
+++ somappy/som_selector_gui.py
@@ -1,11 +1,12 @@
 import sys
 import os
 import shutil
+import json
 
 import PySimpleGUI as sg
 
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import matplotlib.backends.backend_tkagg as tkagg
+from matplotlib.backends.backend_tkagg import FigureCanvasAgg
+import matplotlib.backends.tkagg as tkagg
 import tkinter as Tk
 import matplotlib
 matplotlib.use('TkAgg')
@@ -20,37 +21,41 @@ def draw_figure(canvas, figure, loc=(0, 0)):
     loc: location of top-left corner of figure on canvas in pixels.
     Inspired by matplotlib source: lib/matplotlib/backends/backend_tkagg.py
     """
-    figure_canvas_agg = FigureCanvasTkAgg(figure)
+    figure_canvas_agg = FigureCanvasAgg(figure)
     figure_canvas_agg.draw()
     figure_x, figure_y, figure_w, figure_h = figure.bbox.bounds
     figure_w, figure_h = int(figure_w), int(figure_h)
     photo = Tk.PhotoImage(master=canvas, width=figure_w, height=figure_h)
+    # Position: convert from top-left anchor to center anchor
     canvas.create_image(loc[0] + figure_w/2, loc[1] + figure_h/2, image=photo)
+    # Unfortunately, there's no accessor for the pointer to the native renderer
     tkagg.blit(photo, figure_canvas_agg.get_renderer()._renderer, colormode=2)
+    # Return a handle which contains a reference to the photo object which
+    # must be kept live or else the picture disappears
     return photo
 
 
 def show_figure(fig, som_model_path):
-    figure_x, figure_y, figure_w, figure_h = fig.bbox.bounds    
+    figure_x, figure_y, figure_w, figure_h = fig.bbox.bounds
     print(figure_x, figure_y, figure_w, figure_h)
-    # define the window layout      
-    layout = [[sg.Text('Plot test')],      
+    # define the window layout
+    layout = [[sg.Text('Plot test')],
               [sg.Canvas(size=(figure_w, figure_h), key='canvas')],
               [sg.InputText(key='_save_input_', visible=False, change_submits=True, disabled=True)],
-              [sg.SaveAs('Save Model', enable_events=True, file_types=(("Text Files", "*.txt"),), 
-                        key='_save_model_path_', target='_save_input_'), sg.Cancel(key='_cancel_')]]      
+              [sg.SaveAs('Save Model', enable_events=True, file_types=(("Text Files", "*.txt"),),
+                        key='_save_model_path_', target='_save_input_'), sg.Cancel(key='_cancel_')]]
 
-    # create the window and show it without the plot      
-    fig_window = sg.Window('Demo Application - Embedding Matplotlib In PySimpleGUI').Layout(layout).Finalize()      
+    # create the window and show it without the plot
+    fig_window = sg.Window('Demo Application - Embedding Matplotlib In PySimpleGUI').Layout(layout).Finalize()
 
 
-    # add the plot to the window      
-    fig_photo = draw_figure(fig_window.FindElement('canvas').TKCanvas, fig)      
+    # add the plot to the window
+    fig_photo = draw_figure(fig_window.FindElement('canvas').TKCanvas, fig)
 
     # show it all again and get buttons
     while True:
-        fig_event, fig_values = fig_window.Read()      
-        
+        fig_event, fig_values = fig_window.Read()
+
         print(fig_event)
         if fig_event == '_save_input_':
             # Save the model to their place of choosing
@@ -58,56 +63,63 @@ def show_figure(fig, som_model_path):
             break
         if fig_event == '_cancel_':
             break
-        if event is None:      
+        if event is None:
             break
 
     fig_window.Close()
-            
+
 #root = Tkinter.Tk()
 #defaultbg = root.cget('bg')
 #print(defaultbg)
 
+SETTINGS_KEYS_TO_SAVE = [
+    '_output_dir_path_', '_som_input_path_', '_som_params_path_', '_columns_',
+    '_rows_', '_iterations_', '_grid_type_', '_number_clusters_']
+
 validated = False
 
-layout = [[sg.Text('SOM Selector Tool', size=(30, 1), font=("Helvetica", 14))],      
-[sg.Text('_'  * 100, size=(70, 1))],
-[sg.Text('Workspace:', size=(25, 1), auto_size_text=True, justification='right'), 
-    sg.InputText(do_not_clear=True, key="_output_dir_path_"),
-    sg.FolderBrowse()],
-[sg.Text('_'  * 100, size=(70, 1))],    
-[sg.Text('SOM Input Features:', size=(25, 1), auto_size_text=True, justification='right'), 
-    sg.InputText(do_not_clear=True, key="_som_input_path_"),
-    sg.FileBrowse()],
-[sg.Text('SOM Parameters:', size=(25, 1), auto_size_text=True, justification='right'), 
-    sg.InputText(do_not_clear=True, key='_som_params_path_'),
-    sg.FileBrowse()],
-[sg.Text('SOM Columns:', size=(25, 1), auto_size_text=True, justification='right'), 
-    sg.InputText(do_not_clear=True, key='_columns_')],
-[sg.Text('SOM Rows:', size=(25, 1), auto_size_text=True, justification='right'), 
-    sg.InputText(do_not_clear=True, key='_rows_')],
-[sg.Text('SOM Iterations:', size=(25, 1), auto_size_text=True, justification='right'), 
-    sg.InputText(default_text='500', do_not_clear=True, key='_iterations_')],
-[sg.Text('SOM Grid Type:', size=(25, 1), auto_size_text=True, justification='right'), 
-    sg.InputCombo(['hex', 'square'], default_value=0, key='_grid_type_')],
-[sg.Text('Number of Clusters:', size=(25, 1), auto_size_text=True, justification='right'), 
-    sg.InputText(do_not_clear=True, key='_number_clusters_')],
+menu_def = [['File', ['Load', 'Save', 'Exit',]]]
 
-[sg.Text('', text_color=None, background_color=None, size=(70, 1), key='_success_message_')],
-[sg.Submit(), sg.Cancel(key='_cancel_')]]
+layout = [
+    [sg.Menu(menu_def)],
+    [sg.Text('SOM Selector Tool', size=(30, 1), font=("Helvetica", 14))],
+    [sg.Text('_'  * 100, size=(70, 1))],
+    [sg.Text('Workspace:', size=(25, 1), auto_size_text=True, justification='right'),
+        sg.InputText(do_not_clear=True, key="_output_dir_path_"),
+        sg.FolderBrowse(target = '_output_dir_path_')],
+    [sg.Text('_'  * 100, size=(70, 1))],
+    [sg.Text('SOM Input Features:', size=(25, 1), auto_size_text=True, justification='right'),
+        sg.InputText(do_not_clear=True, key="_som_input_path_"),
+        sg.FileBrowse(target = '_som_input_path_')],
+    [sg.Text('SOM Parameters:', size=(25, 1), auto_size_text=True, justification='right'),
+        sg.InputText(do_not_clear=True, key='_som_params_path_'),
+        sg.FileBrowse(target = '_som_params_path_')],
+    [sg.Text('SOM Columns:', size=(25, 1), auto_size_text=True, justification='right'),
+        sg.InputText(do_not_clear=True, key='_columns_')],
+    [sg.Text('SOM Rows:', size=(25, 1), auto_size_text=True, justification='right'),
+        sg.InputText(do_not_clear=True, key='_rows_')],
+    [sg.Text('SOM Iterations:', size=(25, 1), auto_size_text=True, justification='right'),
+        sg.InputText(default_text='500', do_not_clear=True, key='_iterations_')],
+    [sg.Text('SOM Grid Type:', size=(25, 1), auto_size_text=True, justification='right'),
+        sg.InputCombo(['hex', 'square'], default_value=0, key='_grid_type_')],
+    [sg.Text('Number of Clusters:', size=(25, 1), auto_size_text=True, justification='right'),
+        sg.InputText(do_not_clear=True, key='_number_clusters_')],
+
+    [sg.Text('', text_color=None, background_color=None, size=(70, 1), key='_success_message_')],
+    [sg.Submit(), sg.Cancel(key='_cancel_')]]
 
 window = sg.Window('SOM Selector Tool', auto_size_text=True, default_element_size=(40, 1)).Layout(layout)
 
-# Event Loop      
-while True:      
-    event, values = window.Read()      
-    print(event)
-    if event is None:      
+# Event Loop
+while True:
+    event, values = window.Read()
+    if event is None:
         break
     if event == '_cancel_':
         break
     if event == 'Submit':
         try:
-            # TODO: Should try to validate file inputs first           
+            # TODO: Should try to validate file inputs first
             results = som_selector.execute(values)
             som_fig = results['som_figure']
             som_model_path = results['model_weights_path']
@@ -120,6 +132,28 @@ while True:
         window.FindElement('_success_message_').Update(text_color="#24b73d")
         window.FindElement('_success_message_').Update('The Model Completed Successfully')
         window.FindElement('_cancel_').Update('Close')
-
+    if event == 'Save':
+        save_path = sg.PopupGetFile(
+            'Select a file path to save window parameters.',
+            default_extension = '.json', file_types = (("JSON Files", "*.json"),),
+            no_window = True, save_as = True)
+        if save_path != '':
+            save_key_dict = {}
+            for key, val in values.items():
+                if key in SETTINGS_KEYS_TO_SAVE:
+                    save_key_dict[key] = val
+            with open(save_path, 'w') as fp:
+                json.dump(save_key_dict, fp)
+    if event == 'Load':
+        load_path = sg.PopupGetFile(
+            'Select the Window Parameters to Load.',
+            default_extension = '.json', file_types = (("JSON Files", "*.json"),),
+            no_window = True)
+        if load_path != '':
+            #window.LoadFromDisk(load_path)
+            with open(load_path, 'r') as fp:
+                values = json.load(fp)
+                print(values)
+                window.Fill(values)
 print("Done.")
 
